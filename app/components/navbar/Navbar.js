d95f0d2ac2d2edf7bc7fe0b90628452c9b3648ad
Vincent Dao <congdanhdao95@gmail.com>
2025-07-16 17:15:32 -0700

    update readme.md

---
+++ app/components/navbar/Navbar.js
@@ -0,0 +1,21 @@
+const Navbar = () => {
+	return (
+		<nav>
+			<div>
+				<a href="#home" className="flex item-center gap-2">
+					<p>Velvet Pour</p>
+				</a>
+
+				<ul>
+					{navLinks.map((link) => (
+						<li key={link.id}>
+							<a href={`#${link.id}`}>{link.title}</a>
+						</li>
+					))}
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
