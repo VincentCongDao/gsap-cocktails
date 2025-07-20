8aab019ee99e6fac149fba581d739b5bbc115c57
Vincent Dao <congdanhdao95@gmail.com>
2025-07-20 16:04:46 -0700

    hero implements images, gsap animation for title

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -1,4 +1,26 @@
+"use client";
+import { navLinks } from "@/app/constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 const Navbar = () => {
+	useGSAP(() => {
+		const navTween = gsap.timeline({
+			scrollTrigger: {
+				trigger: "nav",
+				start: "bottom top",
+			},
+		});
+		navTween.fromTo(
+			"nav",
+			{ backgroundColor: "transparent" },
+			{
+				backgroundColor: "#00000050",
+				backgroundFilter: "blur(10px)",
+				duration: 1,
+				ease: "power1.inOut",
+			}
+		);
+	});
 	return (
 		<nav>
 			<div>
--- app/page.js
+++ app/page.js
@@ -1,14 +1,16 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/navbar/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function Home() {
 	return (
-		<div>
-			<div className="flex-center">
-				<h1></h1>
-			</div>
-		</div>
+		<main>
+			<Navbar />
+			<Hero />
+			<div className="h-dvh bg-black"></div>
+		</main>
 	);
 }
