fe7eba5b6c8ce85cf7591393e15dfb0a2e788425
Vincent Dao <congdanhdao95@gmail.com>
2025-07-20 17:17:29 -0700

    Implement hero animation for the cocktails

--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -1,8 +1,13 @@
 "use client";
 import { useGSAP } from "@gsap/react";
 import { gsap, ScrollTrigger, SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+	const videoRef = useRef();
+
+	const isMobile = useMediaQuery({ maxWidth: 767 });
 	gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
 	useGSAP(() => {
 		const heroSplit = new SplitText(".title", {
@@ -33,7 +38,7 @@ const Hero = () => {
 
 		gsap
 			.timeline({
-				ScrollTrigger: {
+				scrollTrigger: {
 					trigger: "#hero",
 					start: "top top",
 					end: "bottom top",
@@ -42,6 +47,28 @@ const Hero = () => {
 			})
 			.to(".right-leaf", { y: 200 }, 0)
 			.to(".left-leaf", { y: -200 }, 0);
+
+		const startValue = isMobile ? "top 50%" : "center 60%";
+
+		const endValue = isMobile ? "120% top" : "bottom top";
+
+		if (videoRef.current) {
+			videoRef.current.onloadedmetadata = () => {
+				const tl = gsap.timeline({
+					scrollTrigger: {
+						trigger: videoRef.current,
+						start: startValue,
+						end: endValue,
+						scrub: true,
+						pin: true,
+					},
+				});
+
+				tl.to(videoRef.current, {
+					currentTime: videoRef.current.duration,
+				});
+			};
+		}
 	}, []);
 
 	return (
@@ -76,6 +103,15 @@ const Hero = () => {
 					</div>
 				</div>
 			</section>
+			<div className="video absolute inset-0">
+				<video
+					ref={videoRef}
+					src="/videos/output.mp4"
+					muted
+					playsInline
+					preload="auto"
+				/>
+			</div>
 		</div>
 	);
 };
